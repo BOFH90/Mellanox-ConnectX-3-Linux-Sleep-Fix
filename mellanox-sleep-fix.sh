53f4295d598a36632f0955ebd12e1f1e07847614
BOFH90 <michael@becker-lan.de>
2021-12-02 09:00:54 +0100

    Update mellanox-sleep-fix.sh

--- mellanox-sleep-fix.sh
+++ mellanox-sleep-fix.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+#lspci | grep Mellanox                                                                                                                     1 ↵
+#XX:XX.X Ethernet controller: Mellanox Technologies MT27500 Family [ConnectX-3]
+#USE YOUR PCIe-ID in the CMD below:
+
 echo 1 >/sys/bus/pci/devices/0000:04:00.0/remove
 echo 1 >/sys/bus/pci/rescan
